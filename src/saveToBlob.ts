95a47a5fe95481721c1f80b78328157d02c46e2f
Analsituation <analsituation@gmail.com>
2024-02-03 14:34:21 +0500

    poopeeter

--- src/saveToBlob.ts
+++ src/saveToBlob.ts
@@ -1,36 +1,38 @@
-import axios from 'axios'
-import crypto from 'crypto'
+// direct send to vercel/blob doesn't work
 
-export function generateSHA1(buffer: Buffer) {
-  return crypto.createHash('sha1').update(buffer).digest('hex')
-}
+// import axios from 'axios'
+// import crypto from 'crypto'
 
-export const uploadToVercel = async (screenshotBuffer: Buffer, digest: string) => {
-  try {
-    const uploadResponse = await axios.post(
-      'https://api.vercel.com/v2/now/files',
-      [
-        {
-          file: screenshotBuffer.toString('base64'),
-          name: 'resume.png',
-          digest
-        }
-      ],
-      {
-        headers: {
-          Authorization: `Bearer Am8fzk393wNIaz2ekaAoKF0S`,
-          'Content-Type': 'application/json'
-        }
-      }
-    )
+// export function generateSHA1(buffer: Buffer) {
+//   return crypto.createHash('sha1').update(buffer).digest('hex')
+// }
 
-    console.log(uploadResponse.data)
-    return uploadResponse.data
+// export const uploadToVercel = async (screenshotBuffer: Buffer, digest: string) => {
+//   try {
+//     const uploadResponse = await axios.post(
+//       'https://api.vercel.com/v2/now/files',
+//       [
+//         {
+//           file: screenshotBuffer.toString('base64'),
+//           name: 'resume.png',
+//           digest
+//         }
+//       ],
+//       {
+//         headers: {
+//           Authorization: `Bearer Am8fzk393wNIaz2ekaAoKF0S`,
+//           'Content-Type': 'application/json'
+//         }
+//       }
+//     )
 
-    // Handle the upload response
-    // The uploadResponse.data will contain information about the uploaded file, such as the URL
-    console.log('Upload successful:', uploadResponse.data)
-  } catch (error) {
-    console.error('Error uploading to Vercel:', error)
-  }
-}
+//     console.log(uploadResponse.data)
+//     return uploadResponse.data
+
+//     // Handle the upload response
+//     // The uploadResponse.data will contain information about the uploaded file, such as the URL
+//     console.log('Upload successful:', uploadResponse.data)
+//   } catch (error) {
+//     console.error('Error uploading to Vercel:', error)
+//   }
+// }
